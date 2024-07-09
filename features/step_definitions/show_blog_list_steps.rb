1a9a974350b1214c6a224778d60359da74c56b63
Antonio Kobashikawa <akobashikawa@gmail.com>
2024-07-09 10:48:30 -0500

    Story 2 show blog list

---
+++ features/step_definitions/show_blog_list_steps.rb
@@ -0,0 +1,17 @@
+Given('there are already {int} posts') do |count|
+    # Given('there are already {float} posts') do |float|
+    count.to_i.times do |n|
+        Post.create!({ :title => "Title #{n}", :content =>
+        "Content #{n}", :post_time => Time.now })
+    end
+end
+
+Given('I am on the homepage') do
+    visit("/posts")
+    # puts page.html
+end
+
+Then('I can see list of {int} posted blogs') do |count|
+    # Then('I can see list of {float} posted blogs') do |float|
+    expect(page).to have_selector("table#posts-list>tr", count: count + 1) # +1 incluye la fila de encabezados
+end
